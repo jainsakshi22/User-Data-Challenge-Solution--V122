18ea438566e677b2fdfd0bfe0834a55b2625a172
Sakshi Jain <sakshi@Sakshis-MacBook-Pro.local>
2014-09-20 20:07:28 +0530

    successfully completed UITableView Solution V144

--- UserDataChallengeSolution-V120-16Sept/CCViewController.h
+++ UserDataChallengeSolution-V120-16Sept/CCViewController.h
@@ -9,8 +9,9 @@
 #import <UIKit/UIKit.h>
 #import "CCUserData.h"
 
-@interface CCViewController : UIViewController
+@interface CCViewController : UIViewController <UITableViewDataSource,UITableViewDelegate>
 
 @property (strong,nonatomic) NSArray *usersArray;
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
 
 @end
--- UserDataChallengeSolution-V120-16Sept/CCViewController.m
+++ UserDataChallengeSolution-V120-16Sept/CCViewController.m
@@ -20,6 +20,8 @@
 	// Do any additional setup after loading the view, typically from a nib.
     self.usersArray = [CCUserData users];
     NSLog(@"%@", self.usersArray);
+    self.tableView.dataSource = self;   //purpose of this line?
+    self.tableView.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
@@ -28,4 +30,22 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UITable Data Source
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.usersArray count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"userCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    NSDictionary *user = [self.usersArray objectAtIndex:indexPath.row];
+    cell.textLabel.text = user[USER_NAME];
+    cell.detailTextLabel.text = user[USER_EMAIL];
+    cell.imageView.image = user[USER_PROFILE_PICTURE];
+    
+    return cell;
+}
 @end
